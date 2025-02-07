b3213a3a076d4fc98b3cb8f0839459ca516f7895
Snehashis Gharai <connectsnehashis22@gmail.com>
2025-02-07 22:39:55 +0530

    Update API URL in .env and enhance vite.config.js with path and React plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   resolve: {
     alias: {

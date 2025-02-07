f8088fd071759297ed0f3ca34e4351e9b0dfad8e
Snehashis Gharai <connectsnehashis22@gmail.com>
2025-02-07 22:32:22 +0530

    Refactor CORS configuration to use a whitelist for allowed origins

--- server/server.js
+++ server/server.js
@@ -17,12 +17,16 @@ const app = express();
 // Enable CORS
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Origin",
-    ],
+    origin: function (origin, callback) {
+      if (!origin) {
+        return callback(null, true);
+      }
+      if (process.env.WHITELIST.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     maxAge: 86400,
